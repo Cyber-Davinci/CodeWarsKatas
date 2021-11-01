e37bfcabb5ed83eb68d29bc49700efbed51e8612
Cyber-Davinci <78156095+Cyber-Davinci@users.noreply.github.com>
2021-11-01 07:57:33 -1200

    added few comments

--- katas_challenges/two_sum.py
+++ katas_challenges/two_sum.py
@@ -6,10 +6,12 @@ For the purposes of this kata, some tests may have multiple answers; any valid s
 The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).
 """
 def two_sum(numbers, target):
+	# long and very efficient
 	for x in numbers:
 		for y in numbers:
 			if x != y and x+y == target:
 				return tuple([numbers.index(x), numbers.index(y)])
+	# short but not efficient
 	# ok = [numbers.index(x) for x in numbers for y in numbers if x!=y and x+y==target]
 	# return tuple(ok)
 
